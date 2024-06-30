8b71831430fbb7188932cc986883b9ad356f6a7e
Kava <nikolaitarasov2121@mail.ru>
2024-06-30 09:29:58 +0300

    smooth-scroll

--- src/js/app.js
+++ src/js/app.js
@@ -2,5 +2,7 @@ import "../scss/style.scss";
 import { burger } from "./files/burger.js";
 
 import "./files/slider.js";
+import { smoothScroll } from "./files/smooth-scroll.js";
 
 burger();
+smoothScroll();
